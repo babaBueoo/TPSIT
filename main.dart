16f1ae22dd4091d99e649dc66d94b08d3c050066
babaBueoo <mattia.barina@itiszuccante.edu.it>
2025-10-30 18:39:22 +0100

    Update main.dart
    sistemati sfondi bottoni
    aggiunti:
    timedCounter per generare numero
    pauseCounter  e startCounter per creare e aggiornare e mettere in pausa il contatore(stream)
    resume e stop Counter

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -24,14 +25,22 @@ class _MyHomePageState extends State<MyHomePage> {
   String message1 = "Start";
   String message2 = "Pause";
   List<Color> colori1 = [Colors.green, Colors.red, Colors.yellow];
-  List<Color> colori2 = [Colors.green, Colors.red, Colors.yellow];
+  List<Color> colori2 = [Colors.red, Colors.green];
   int statoBottone1 = 0;
   int statoBottone2 = 0;
+
+  bool schiacciato = false;
 
   void cambiaTesto1() {
     const List<String> bottone1 = ["Start", "Stop", "Restart"];
     setState(() {
       message1 = bottone1[(bottone1.indexOf(message1) + 1) % bottone1.length];
+
+      if (message1 == "Stop" || message1 == "Restart") {
+        schiacciato = true;
+      } else if (message1 == "Start") {
+        schiacciato = false;
+      }
     });
   }
 
@@ -42,68 +51,132 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-void cambiaColore2() {
+  void cambiaColore2() {
     setState(() {
       statoBottone2 = (statoBottone2 + 1) % colori2.length;
     });
   }
+
   void cambiaColore1() {
     setState(() {
-      statoBottone1 = (statoBottone1 + 1) % colori2.length;
+      statoBottone1 = (statoBottone1 + 1) % colori1.length;
+    });
+  }
+
+  int counter = 0;
+  Stream<int>? counterStream;
+  StreamSubscription<int>? subscription;
+
+  Stream<int> timedCounter(Duration interval) async* {
+    int i = 0;
+    while (true) {
+      await Future.delayed(interval);
+      yield i++;
+    }
+  }
+
+  void startCounter() {
+    subscription?.cancel();
+    counterStream = timedCounter(const Duration(seconds: 1));
+    subscription = counterStream!.listen((value) {
+      setState(() {
+        counter = value;
+      });
+    });
+  }
+
+  void pauseCounter() {
+    subscription?.pause();
+  }
+
+  void resumeCounter() {
+    subscription?.resume();
+  }
+
+  void stopCounter() {
+    subscription?.cancel();
+    setState(() {
+      counter = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.bottomRight,
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              FloatingActionButton(
-                backgroundColor: Colors.brown.shade50,
-                foregroundColor: Colors.blueGrey.shade600,
-                onPressed: cambiaTesto2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(iconaMessaggio2(), color: Colors.blueGrey.shade600),
-                    Text(
-                      message2,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.blueGrey.shade600,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(width: 30),
-              FloatingActionButton(
-                backgroundColor: Colors.brown.shade50,
-                foregroundColor: Colors.blueGrey.shade600,
-                onPressed: cambiaTesto1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(iconaMessaggio1(), color: Colors.blueGrey.shade600),
-                    Text(
-                      message1,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.blueGrey.shade600,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      body: Stack(
+        children: [
+          Center(
+            child: Text(
+              '$counter',
+              style: const TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  FloatingActionButton(
+                    backgroundColor: schiacciato
+                        ? colori2[statoBottone2]
+                        : Colors.brown.shade50,
+                    foregroundColor: Colors.blueGrey.shade600,
+                    onPressed: schiacciato
+                        ? () {
+                            cambiaTesto2();
+                            cambiaColore2();
+                          }
+                        : null,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          iconaMessaggio2(),
+                          color: Colors.blueGrey.shade600,
+                        ),
+                        Text(
+                          message2,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.blueGrey.shade600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  FloatingActionButton(
+                    backgroundColor: colori1[statoBottone1],
+                    foregroundColor: Colors.blueGrey.shade600,
+                    onPressed: () {
+                      cambiaColore1();
+                      cambiaTesto1();
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          iconaMessaggio1(),
+                          color: Colors.blueGrey.shade600,
+                        ),
+                        Text(
+                          message1,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.blueGrey.shade600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
