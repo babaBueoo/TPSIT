13d544df133497e00c98673c6f8fbd1019e1a486
babaBueoo <mattia.barina@itiszuccante.edu.it>
2025-11-10 11:45:37 +0100

    Update main.dart

--- chatroom/flutter_z41sa06en41sb/lib/main.dart
+++ chatroom/flutter_z41sa06en41sb/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'dart:io';
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,33 +10,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: const MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-  void main() {
-  Socket.connect("127.0.0.1", 3000).then((socket) {
-    print('Connected to: '
-        '${socket.remoteAddress.address}:${socket.remotePort}');
-    socket.destroy();
-  }).catchError((e) {
-    // server down
-    if (e is SocketException) print('SocketException => $e');
-  });
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class _MyHomePageState extends State<MyHomePage> {
+  String _connectionStatus = "Disconnected";
 
-class _MyHomePageState extends State<MyHomePage>{
+  @override
+  void initState() {
+    super.initState();
+    // 
+    _connectToServer();
+  }
 
+  
+  void _connectToServer() {
+    // Logica di connessione non completa
+    String indexRequest = 'GET / HTTP/1.1\nConnection: close\n\n';
+    // Socket.connect("localhost", 3000).then((socket) {
+    //   
+    // }).catchError((e) {
+    // 
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text("Flutter Socket Connection")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Socket Connection Status:',
+              style: Theme.of(context),
+            ),
+            Text(
+              _connectionStatus,
+              style: Theme.of(context).textTheme(
+                    color: _connectionStatus == "Connected"
+                        ? Colors.green
+                        : (_connectionStatus == "Connection Failed"
+                            ? Colors.red
+                            : Colors.grey),
+                  ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
