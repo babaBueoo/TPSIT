5a94324d85c319b0b70c6183bd9319f3f4706aca
babaBueoo <mattia.barina@itiszuccante.edu.it>
2026-01-12 11:50:55 +0100

    Add files via upload

---
+++ temporaneo/widgets.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'model.dart';
+import 'notifier.dart';
+
+class TodoItem extends StatelessWidget {
+  TodoItem({required this.todo}) : super(key: ObjectKey(todo));
+
+  final Todo todo;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+
+    return const TextStyle(
+      color: Colors.black45,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TodoListNotifier notifier = context.watch<TodoListNotifier>();
+
+    return ListTile(
+      leading: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Checkbox(value: todo.checked, onChanged: (_){
+            notifier.changeTodo(todo);
+          },
+          ),
+        ],
+      ),
+      title: Text(
+        todo.name,
+        style: _getTextStyle(todo.checked),
+      ),
+      onTap: () {
+        notifier.changeTodo(todo);
+      },
+      onLongPress: () {
+        notifier.deleteTodo(todo);
+      },
+    );
+  }
+}
+
+
+
