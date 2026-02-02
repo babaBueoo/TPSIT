a0184110ad8cb0e0c5415a897fde0ab85c994645
babaBueoo <mattia.barina@itiszuccante.edu.it>
2026-02-02 11:28:56 +0100

    Update model.dart

--- keep/lib/model.dart
+++ keep/lib/model.dart
@@ -1,6 +1,16 @@
-
 class Todo {
-  Todo({required this.name, required this.checked});
-  final String name;
+  Todo({required this.name, required this.notes});
+  String name;
+  List<TodoNote> notes;
+
+  @override
+  String toString() {
+    return name;
+  }
+}
+
+class TodoNote {
+  TodoNote({required this.text, required this.checked});
+  String text;
   bool checked;
 }
