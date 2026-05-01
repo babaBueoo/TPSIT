b8ba313a6d59e3e2d64a3bfc97d3c3019fcbf917
babaBueoo <mattia.barina@itiszuccante.edu.it>
2026-05-01 15:57:44 +0200

    Update parking_repository.dart

--- babaxpark/lib/repositories/parking_repository.dart
+++ babaxpark/lib/repositories/parking_repository.dart
@@ -1,4 +1,4 @@
-import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'dart:io';
 import '../models/veicolo.dart';
 import '../models/prenotazione.dart';
 import '../services/api_service.dart';
@@ -8,80 +8,63 @@ class ParkingRepository {
   final ApiService apiService = ApiService();
   final DatabaseHelper dbHelper = DatabaseHelper.instance;
 
-  Future<bool> hasConnection() async {
-    return await InternetConnection().hasInternetAccess;
-  }
-
   // --- VEICOLI ---
   Future<List<Veicolo>> getVeicoli() async {
-    if (await hasConnection()) {
-      try {
-        final veicoli = await apiService.getVeicoli();
-        // Sincronizza cache locale
-        await dbHelper.clearVeicoli();
-        for (var v in veicoli) {
-          await dbHelper.insertVeicolo(v);
-        }
-        return veicoli;
-      } catch (e) {
-        // caricamennto dati da database locale in caso di errore API
-        return await dbHelper.getAllVeicoli();
+    try {
+      final veicoli = await apiService.getVeicoli();
+      // sincfonizza chace locale
+      await dbHelper.clearVeicoli();
+      for (var v in veicoli) {
+        await dbHelper.insertVeicolo(v);
       }
-    } else {
+      return veicoli;
+    } catch (e) {
+     //offline o errore server
       return await dbHelper.getAllVeicoli();
     }
   }
 
   Future<Veicolo> createVeicolo(Veicolo veicolo) async {
-    if (await hasConnection()) {
+    try {
       final createdVeicolo = await apiService.createVeicolo(veicolo);
       await dbHelper.insertVeicolo(createdVeicolo);
       return createdVeicolo;
-    } else {
+    } on SocketException {
       throw Exception('Operazione bloccata: impossibile creare un veicolo offline.');
     }
   }
 
   // --- PRENOTAZIONI ---
   Future<List<Prenotazione>> getPrenotazioni() async {
-    if (await hasConnection()) {
-      try {
-        final prenotazioni = await apiService.getPrenotazioni();
-        // Sincronizza cache locale
-        await dbHelper.clearPrenotazioni();
-        for (var p in prenotazioni) {
-          await dbHelper.insertPrenotazione(p);
-        }
-        return await dbHelper.getPrenotazioni();
-      } catch (e) {
-        // caricamennto dati da database locale in caso di errore API
-        return await dbHelper.getPrenotazioni();
+    try {
+      final prenotazioni = await apiService.getPrenotazioni();
+      // wincronizza cache locale
+      await dbHelper.clearPrenotazioni();
+      for (var p in prenotazioni) {
+        await dbHelper.insertPrenotazione(p);
       }
-    } else {
-      
+      return await dbHelper.getPrenotazioni();
+    } catch (e) {
+      // offline o errore server
       return await dbHelper.getPrenotazioni();
     }
   }
 
   Future<Prenotazione> createPrenotazione(Prenotazione prenotazione) async {
-    if (await hasConnection()) {
+    try {
       final createdPrenotazione = await apiService.createPrenotazione(prenotazione);
       await dbHelper.insertPrenotazione(createdPrenotazione);
       return createdPrenotazione;
-    } else {
+    } on SocketException {
       throw Exception('Operazione bloccata: impossibile creare una prenotazione offline.');
     }
   }
 
   Future<void> deletePrenotazione(String id) async {
-    if (await hasConnection()) {
-      try {
-        await apiService.deletePrenotazione(id);
-      } catch (e) {
-
-      }
+    try {
+      await apiService.deletePrenotazione(id);
       await dbHelper.deletePrenotazione(id);
-    } else {
+    } on SocketException {
       throw Exception('Operazione bloccata: impossibile cancellare una prenotazione offline.');
     }
   }
