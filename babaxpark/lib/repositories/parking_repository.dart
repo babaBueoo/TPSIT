32aa722046c703e57a88c6418b5df9db987968d2
babaBueoo <mattia.barina@itiszuccante.edu.it>
2026-05-03 22:56:35 +0200

    Update parking_repository.dart

--- babaxpark/lib/repositories/parking_repository.dart
+++ babaxpark/lib/repositories/parking_repository.dart
@@ -11,17 +11,14 @@ class ParkingRepository {
   // --- VEICOLI ---
   Future<List<Veicolo>> getVeicoli() async {
     try {
-<<<<<<< HEAD
       List<Veicolo> veicoli = await apiService.getVeicoli();
       // sincfonizza chace locale
       await dbHelper.clearVeicoli();
       for (Veicolo v in veicoli) {
-=======
       final veicoli = await apiService.getVeicoli();
       // sincfonizza chace locale
       await dbHelper.clearVeicoli();
-      for (var v in veicoli) {
->>>>>>> b8ba313a6d59e3e2d64a3bfc97d3c3019fcbf917
+      for (Veicolo v in veicoli) {
         await dbHelper.insertVeicolo(v);
       }
       return veicoli;
@@ -33,11 +30,8 @@ class ParkingRepository {
 
   Future<Veicolo> createVeicolo(Veicolo veicolo) async {
     try {
-<<<<<<< HEAD
       Veicolo createdVeicolo = await apiService.createVeicolo(veicolo);
-=======
       final createdVeicolo = await apiService.createVeicolo(veicolo);
->>>>>>> b8ba313a6d59e3e2d64a3bfc97d3c3019fcbf917
       await dbHelper.insertVeicolo(createdVeicolo);
       return createdVeicolo;
     } on SocketException {
@@ -48,17 +42,14 @@ class ParkingRepository {
   // --- PRENOTAZIONI ---
   Future<List<Prenotazione>> getPrenotazioni() async {
     try {
-<<<<<<< HEAD
       List<Prenotazione> prenotazioni = await apiService.getPrenotazioni();
       // wincronizza cache locale
       await dbHelper.clearPrenotazioni();
       for (Prenotazione p in prenotazioni) {
-=======
       final prenotazioni = await apiService.getPrenotazioni();
       // wincronizza cache locale
       await dbHelper.clearPrenotazioni();
-      for (var p in prenotazioni) {
->>>>>>> b8ba313a6d59e3e2d64a3bfc97d3c3019fcbf917
+      for (Prenotazione p in prenotazioni) {
         await dbHelper.insertPrenotazione(p);
       }
       return await dbHelper.getPrenotazioni();
@@ -70,11 +61,8 @@ class ParkingRepository {
 
   Future<Prenotazione> createPrenotazione(Prenotazione prenotazione) async {
     try {
-<<<<<<< HEAD
       Prenotazione createdPrenotazione = await apiService.createPrenotazione(prenotazione);
-=======
       final createdPrenotazione = await apiService.createPrenotazione(prenotazione);
->>>>>>> b8ba313a6d59e3e2d64a3bfc97d3c3019fcbf917
       await dbHelper.insertPrenotazione(createdPrenotazione);
       return createdPrenotazione;
     } on SocketException {
